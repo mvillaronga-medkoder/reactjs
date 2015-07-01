b097f2225a3be7085cd0b2acf10ac0b1b0c5f6d7
mvillaronga-medkoder <mvillaronga@medkoder.com>
2015-07-01 11:57:30 -0500

    Stuff

---
+++ reactjs.web/reactjs.web/Areas/Components/Controllers/CommentController.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+using Newtonsoft.Json;
+
+using reactjs.web.Areas.Components.Models;
+using reactjs.web.services;
+using reactjs.web.domain;
+using System;
+
+namespace reactjs.web.Areas.Components.Controllers
+{
+    public class CommentController : Controller
+    {
+        RepositoryMock repo = new RepositoryMock();
+
+        public CommentController() {
+        }
+
+        // GET: Components/Comment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET:  Components/Comment/List
+
+        [HttpGet]
+        public JsonResult List()
+        {
+            JsonResult ret = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+            List<CommentModel> comments = new List<CommentModel>();
+
+            repo.Query<Comment>().ToList().ForEach(c => comments.Add(new CommentModel().fromDomain(c)));
+
+            ret = Json(comments);
+            ret.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+            return ret;
+        }
+
+        [HttpPost]
+        public JsonResult Add(string text)
+        {
+            JsonResult ret = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+            repo.Save(new Comment() {
+             Author = "Villaronga, Michael", //from some app context server
+             CommentText = text,
+             CreatedDate = DateTime.Now
+            });
+
+
+            List<CommentModel> comments = new List<CommentModel>();
+            repo.Query<Comment>().ToList().ForEach(c => comments.Add(new CommentModel().fromDomain(c)));
+            ret = Json(comments);
+            ret.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+            return ret;
+        }
+
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            JsonResult ret = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+            Comment cmt = repo.GetById<Comment>(id);
+            repo.Delete(cmt);
+
+            List<CommentModel> comments = new List<CommentModel>();
+            repo.Query<Comment>().ToList().ForEach(c => comments.Add(new CommentModel().fromDomain(c)));
+            ret = Json(comments);
+            ret.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+            return ret;
+        }
+    }
+}
